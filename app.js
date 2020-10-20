ba873a2074c16f300c2dc35597f49515be226b33
XYIAN <kdilbeck@csumb.edu>
2020-10-20 22:52:32 +0000

    master rollback bff82360bef33ff5419ea248d19603d38a4543d2

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var session = require('express-session');
 var passport = require('passport');
 var bodyParser = require('body-parser');
 
+var cart_qty =0;
+var total = 0;
 app.engine('html', require('ejs').renderFile);//render other files
 app.use(express.static("public"));//access img css js or any external file
 app.use(express.static('css'));
@@ -82,6 +84,8 @@ app.get("/login", function(req, res){ // login route
     res.render("login.ejs", {loginError: false});
 });
 
+
+
 app.post('/login', function(req, res){
     var stmt = 'select * from user_info where userName=\'' 
                 +req.body.un+'\' '+
@@ -92,7 +96,10 @@ app.post('/login', function(req, res){
         if(results.length){      //user is in db
             user = results[0].userName;
             req.session.login = user;
-            res.redirect('/');        
+            res.redirect('/');
+            
+            res.render('login.ejs', {loggedIn: true});//show cart  raul
+            
         }else {                        //user is not in db - do this as a pop up later
             console.log("Incorrect Login Info");
             res.render('login.ejs', {loginError: true});
@@ -105,12 +112,13 @@ app.get('/logout', function(req, res){
     if (req.session.login != null) {
         var name = req.session.login;     
         req.session.destroy();
+        req.body.loggedIn = false;// raul
         res.render("logout.ejs", {logoutUser: name});
     }else res.render("logout.ejs",{logoutUser: "ERROR NO LOG IN"})    
 });
 
-app.get("/signUp", function(req, res){ // sign up route
-    res.render("signUp.ejs");
+app.post("/signUp", function(req, res){ // sign up route
+    //res.render("signUp.ejs");
     var user = req.body.newUsername;
     var password = req.body.newPassword;
 
@@ -125,7 +133,7 @@ app.get("/signUp", function(req, res){ // sign up route
         new_user = user;
         new_password = password;
         req.session.signUp = user;
-        var random = Math.floor(Math.random() * 100);
+        var random = Math.floor(Math.random() * 1000);
         
         var sql_data = 'insert * into user_info where userName\''
                         + req.body.new_user +'\' '
@@ -143,6 +151,26 @@ app.get("/signUp", function(req, res){ // sign up route
     
 });
 
+<<<<<<< HEAD
+app.post('/', function(req, res) {//index?
+    
+    var stmt = 'select * from bookInfo where bookId\''
+                    + req.body.id + '\'' 
+                    + 'and inStock=\'' +
+                    req.body.instock + '\''
+                    + 'and price=\'' +
+                    req.body.price+'\'';
+                    
+    connection.query(stmt, function(error, results) {
+        if(results[0].inStock ==0){
+            cart_qty += 1;
+            total = results[0].pricea + total;
+            
+        }
+    })
+    
+});
+=======
 app.get("/search", function(req,res) //search route
 {
     var stmt = "select cover, title, author, year, price from book_info;";
@@ -157,6 +185,7 @@ app.get("/search", function(req,res) //search route
 });
 
 
+>>>>>>> bff82360bef33ff5419ea248d19603d38a4543d2
 
 //server listener - run server w/ port number
 //8081(have to include in url) , "0, 0 , 0 , 0" -used for php type
